b33000943cfa6805a7d479eeb623e4ebeef65494
Gints Polis <polis.gints@gmail.com>
2023-05-05 11:43:23 +0300

    changes to call any BDT

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 use tonic::{transport::Server, Request, Response, Status};
 
-use tt_config::tt_config_server::{TtConfig, TtConfigServer};
-use tt_config::{BdtRequest, TtConfigObject};
+use bdt::bdt_server::{Bdt, BdtServer};
+use bdt::{BdtRequest, BdtResponse, Row};
 
-pub mod tt_config {
-    tonic::include_proto!("tt.config");
+pub mod bdt {
+    tonic::include_proto!("bdt");
 }
 
 #[derive(Debug, Default)]
-pub struct TtConfigService {}
+pub struct BdtService {}
 
 #[tonic::async_trait]
-impl TtConfig for TtConfigService {
+impl Bdt for BdtService {
     async fn get_data(
         &self,
         request: Request<BdtRequest>,
-    ) -> Result<Response<TtConfigObject>, Status> {
+    ) -> Result<Response<BdtResponse>, Status> {
         println!("Got a request: {:?}", request);
 
         let req = request.into_inner();
 
-        let reply = TtConfigObject {
-            config_type: "type".to_string(),
-            config_value: format!("Select: {:?}", req),
-            config_num_value: 0.3,
+        let row1 = Row {
+            values: vec![]
+        };
+
+        let reply = BdtResponse {
+            rows: vec![row1]
         };
 
         Ok(Response::new(reply))
@@ -33,10 +35,10 @@ impl TtConfig for TtConfigService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let tt_config_service = TtConfigService::default();
+    let bdt_service = BdtService::default();
 
     Server::builder()
-        .add_service(TtConfigServer::new(tt_config_service))
+        .add_service(BdtServer::new(bdt_service))
         .serve(addr)
         .await?;
 
